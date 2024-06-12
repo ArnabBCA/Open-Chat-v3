9138b58775b5ec60b7d2048b705da4c9ee8049a5
Arnab Ghosh <arnabsonu2@gmail.com>
2024-06-13 00:11:45 +0530

    Added Logged User Presence Status feature

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,10 +1,53 @@
+import { useSelector } from 'react-redux';
 import LeftConainer from '../LeftConainer';
 import RightContainer from '../RightContainer';
 import Sidebar from '../Sidebar';
+import {
+  getDatabase,
+  onDisconnect,
+  onValue,
+  ref,
+  serverTimestamp,
+  update,
+} from 'firebase/database';
+import { useEffect } from 'react';
 
 const Home = () => {
+  const currentUser = useSelector((state: RootState) => state.currentUser);
+  const db = getDatabase();
+  const userConnectionsRef = ref(db, `users/${currentUser.uid}`);
+  const connectedRef = ref(db, '.info/connected');
+
+  const handleVisibilityChange = () => {
+    document.addEventListener('visibilitychange', handleVisibilityChange);
+    onValue(connectedRef, (snap) => {
+      if (snap.val() === true) {
+        if (document.visibilityState === 'hidden') {
+          update(userConnectionsRef, {
+            status: 'away',
+          });
+        } else {
+          update(userConnectionsRef, {
+            status: 'online',
+          });
+        }
+        onDisconnect(userConnectionsRef).update({
+          status: 'offline',
+          lastOnline: serverTimestamp(),
+        });
+      }
+    });
+  };
+
+  useEffect(() => {
+    handleVisibilityChange();
+    return () => {
+      document.removeEventListener('visibilitychange', handleVisibilityChange);
+    };
+  }, []);
+
   return (
-    <div className="relative flex h-svh w-full bg-white overflow-x-hidden">
+    <div className="relative flex h-svh w-full overflow-x-hidden">
       <Sidebar />
       <LeftConainer />
       <RightContainer />
--- src/firebase.ts
+++ src/firebase.ts
@@ -6,12 +6,14 @@ import { getStorage } from 'firebase/storage';
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
   projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
   measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);

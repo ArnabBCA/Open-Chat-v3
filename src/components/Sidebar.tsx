29d35a6d4f47d873cf9b8df447e33981e7eaa8e4
Arnab Ghosh <arnabsonu2@gmail.com>
2024-06-12 14:23:04 +0530

    Added Profile Info In Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -21,6 +21,8 @@ const Sidebar = () => {
   const dispatch = useDispatch();
   const theme = useSelector((state) => state.theme);
   const isSidebarOpen = useSelector((state) => state.isSidebarOpen);
+  const currentUser = useSelector((state) => state.currentUser);
+  console.log(currentUser);
 
   const handleExpandClick = () => {
     dispatch(toggleSidebar());
@@ -31,15 +33,30 @@ const Sidebar = () => {
   };
 
   return (
-    <div className="t relative h-full min-w-16">
+    <div className="relative h-full min-w-16">
       <CheckOutsideClick handleClose={handleExpandClick} isOpen={isSidebarOpen}>
         <div
           className={`absolute flex h-full flex-col justify-between gap-2 overflow-y-scroll bg-sidebar p-2 duration-300 ${isSidebarOpen ? 'w-60' : 'w-16'}`}
         >
           <div className="flex flex-col gap-2">
-            <SidebarButtons onClick={() => {}} lable="Home">
-              <IoHome />
-            </SidebarButtons>
+            <div
+              onClick={() => {}}
+              className="group relative flex w-full cursor-pointer items-center gap-2"
+            >
+              <button className="flex h-12 w-12 items-center justify-center rounded-3xl text-2xl">
+                <img
+                  className="rounded-3xl duration-300 hover:rounded-2xl"
+                  referrerPolicy="no-referrer"
+                  src={currentUser.photoURL}
+                  alt="Profile Pic"
+                />
+              </button>
+              <span
+                className={`absolute whitespace-nowrap text-base text-inputText ${isSidebarOpen ? 'translate-x-14' : 'hidden'}`}
+              >
+                {currentUser.displayName}
+              </span>
+            </div>
             <SidebarButtons onClick={() => {}} lable="Chats">
               <IoChatbubbles />
             </SidebarButtons>

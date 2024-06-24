22f89f8d61f02937ef3b76b5b207ff0a7f5cbd60
Arnab Ghosh <arnabsonu2@gmail.com>
2024-06-24 13:47:17 +0530

    CSS fix

--- src/components/LeftConainer.tsx
+++ src/components/LeftConainer.tsx
@@ -3,8 +3,10 @@ import Search from './Search';
 
 const LeftConainer = () => {
   return (
-    <div className="h-full w-full  bg-left p-2 transition-colors duration-300 sm:max-w-80 flex flex-col gap-2">
-      <h1 className='text-inputText text-2xl'>Chats</h1>
+    <div
+      className={`flex h-full w-[calc(100%-7rem)] flex-col gap-2 bg-left p-2 transition-colors duration-300 sm:max-w-80`}
+    >
+      <h1 className="text-2xl text-inputText">Chats</h1>
       <Search />
       <Contacts />
     </div>
